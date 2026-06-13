5d152be16c434e25bc5a4b566f1ede27de044d94
Thifany M. Murakami <thifanymurakami28@gmail.com>
2026-06-13 17:03:32 -0300

    atualizado

--- externo.js
+++ externo.js
@@ -1,5 +1,13 @@
 function horaAtual() {
     const agora = new Date();
-    document.getElementById("hora").innerText = "Hora atual: " + agora.toLocaleTimeString();
+    document.getElementById("hora").innerText =
+        "Hora atual: " + agora.toLocaleTimeString();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .getElementById("btnHora")
+        .addEventListener("click", horaAtual);
+});
+
 console.log("Script externo carregado!");
